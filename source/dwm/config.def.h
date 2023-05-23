42e27aab73c30b07c5ca81055e7f49d30bb6652f
Safal Piya <mrsafalpiya@gmail.com>
2023-05-23 22:05:30 +0545

    Made border to 2px

--- source/dwm/config.def.h
+++ source/dwm/config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 16;        /* gaps between windows */
 static const unsigned int snap      = 35;       /* snap pixel */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
