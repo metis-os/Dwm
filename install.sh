b06937d2a2d027aa5332062de163dfade5643f6c
Isaac Turner <spacey_sooty@outlook.com>
2023-11-27 21:59:24 +0800

    add install script for debian based operating systems

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt install dwm libx11-dev libxinerama-dev libxft-dev libx11-xcb-dev libxcb-res0-dev libx11-dev
+cd source/dwm
+sudo make install
+cd source/dmenu
+sudo make install
+cd source/slock
+sudo make install
+cd source/st
+sudo make install
