6ac15ede1cfac3a6bc6f655da60f2aade3cfaa3d
Isaac Turner <spacey_sooty@outlook.com>
2023-11-28 11:29:42 +0800

    fix cd to root

--- install-deb.sh
+++ install-deb.sh
@@ -1,9 +1,13 @@
 sudo apt install dwm libx11-dev libxinerama-dev libxft-dev libx11-xcb-dev libxcb-res0-dev libx11-dev
 cd source/dwm
 sudo make install
+cd ..
 cd source/dmenu
 sudo make install
+cd ..
 cd source/slock
 sudo make install
+cd ..
 cd source/st
 sudo make install
+cd ..
